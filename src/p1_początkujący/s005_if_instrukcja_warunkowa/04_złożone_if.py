999dc8dc789e7cfceba3f1b373a6e7f123d764ee
Cezary K. Wagner <Cezary.Wagner@gmail.com>
2022-04-25 01:23:10 +0200

    Fixed doctest.

--- src/p1_początkujący/s005_if_instrukcja_warunkowa/04_złożone_if.py
+++ src/p1_początkujący/s005_if_instrukcja_warunkowa/04_złożone_if.py
@@ -1,14 +1,20 @@
-x = True
-y = False
-z = True
+"""
+Złożone warunki
+===============
 
-if (x is True
-        and (y is False
-             or z is False)):
-    print(True)
+>>> x = True
+>>> y = False
+>>> z = True
+>>>
+>>> if (x is True
+...         and (y is False
+...              or z is False)):
+...     print(True)
+True
 
-# Lepiej takie warunki rozbijać dla czytelności.
-# Nie zawsze trzeba się tego trzymać na sztywno.
+Lepiej takie warunki rozbijać dla czytelności.
+Nie zawsze trzeba się tego trzymać na sztywno.
+"""
 
 if __name__ == '__main__':
     import doctest
