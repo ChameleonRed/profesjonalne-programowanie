72617b66aeebe70163372369915745bb083544f2
Cezary K. Wagner <Cezary.Wagner@gmail.com>
2022-05-04 15:28:44 +0200

    Added missed output.

--- src/p1_początkujący/s006_pętla_for/05_wielokrotne_for.py
+++ src/p1_początkujący/s006_pętla_for/05_wielokrotne_for.py
@@ -73,6 +73,18 @@ Skok z raise
 ...                 raise StopIteration()
 ... except StopIteration:
 ...     pass
+1 + 1 = 2
+1 + 2 = 3
+1 + 3 = 4
+1 + 4 = 5
+1 + 5 = 6
+1 + 6 = 7
+2 + 1 = 3
+2 + 2 = 4
+2 + 3 = 5
+2 + 4 = 6
+2 + 5 = 7
+2 + 6 = 8
 
 Można też użyć return, jeśli chcemy wyjść z pętli i funkcji.
 """
