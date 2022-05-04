4f9f7fa7f2ee92c3a3208529a4bccf6587eb4637
Cezary K. Wagner <Cezary.Wagner@gmail.com>
2022-05-04 19:10:11 +0200

    Little more complex for.

--- src/p1_początkujący/s006_pętla_for/00_podstawy_for.py
+++ src/p1_początkujący/s006_pętla_for/00_podstawy_for.py
@@ -1,7 +1,13 @@
 lista_wartości = [0, 1, 2, 3, 4]
 
 for wartość in lista_wartości:
-    print(wartość)
+    if wartość % 2 == 0:
+        print(f'{wartość} jest parzyste')
+        continue
+    if wartość % 3 == 0:
+        print(f'{wartość} jest podzielne przez 3')
+        break
+
 
 for i in range(5):
     print(i)
