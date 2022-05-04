3ad4109fb9ea55f05d286ce1b7f7999b9c5c86ec
Cezary K. Wagner <Cezary.Wagner@gmail.com>
2022-05-04 19:10:54 +0200

    Added comments.

--- src/p1_początkujący/s006_pętla_for/00_podstawy_for.py
+++ src/p1_początkujący/s006_pętla_for/00_podstawy_for.py
@@ -1,9 +1,11 @@
 lista_wartości = [0, 1, 2, 3, 4]
 
 for wartość in lista_wartości:
+    # drukuj parzyste
     if wartość % 2 == 0:
         print(f'{wartość} jest parzyste')
         continue
+    # wyjdź z pętli jak podzielne przez 3
     if wartość % 3 == 0:
         print(f'{wartość} jest podzielne przez 3')
         break
