65b1ae8f95417fc01c6c75607254ec45d71bf4aa
Cezary K. Wagner <Cezary.Wagner@gmail.com>
2022-06-18 16:16:06 +0200

    Moved code from sandbox.

---
+++ src/dobry, zły i głupi/dzg001_nazwy_zmiennych.py
@@ -0,0 +1,52 @@
+# pseudopośpiech
+
+# kalki matematyczne
+
+a = 1.0
+b = 2.0
+c = 3.0
+
+# kalki fizyczne
+
+t = 1.0  # Co to jest t?
+a = 10
+v0 = 0
+v = v0 + a * t
+s0 = 1.0
+s = s0 + v0 * t + a * t ** 2 / 2
+
+# Programowanie to nie fizyka i matematyka.
+
+# pseudoskróty
+# pseudokompresja
+
+wsp_a = 1.0  # co to jest?
+
+# Czemu to takie złe?
+# Kiedy jest to użyteczne?
+# Czemu zwykle tak nie kodują profesjonaliści?
+
+# prawidłowo
+
+czas = 1.0
+przyśpieszenie = 1.0
+prędkość_początkowa = 1.0
+droga_początkowa = 1.0
+prędkość = przyśpieszenie * czas
+droga = droga_początkowa + prędkość_początkowa * czas + przyśpieszenie * czas ** 2.0 / 2.0
+
+# Czemu to takie dobre?
+
+# Jak się wymyśla nazwy zmiennych?
+# https://synonim.net/ - po polsku.
+# https://www.thesaurus.com/ - po angielsku.
+
+# W jakim języku nazywać zmienne?
+
+
+
+
+
+
+
+
---
+++ src/dobry, zły i głupi/dzg002_reguła_kiss.py
@@ -0,0 +1,37 @@
+# KISS
+
+
+# Keep It Simple Stupid
+# Trzymaj się prostoty głupcze
+
+# U.S. Navy 1960
+# https://en.wikipedia.org/wiki/KISS_principle
+
+
+# Nic nowego dla wielu ludzi sukcesu :)
+
+# Marek Aureliusz - Cesarz rzymski
+# Zawsze wybieraj drogę najkrótszą, albowiem krótka droga jest naturalna
+
+# Leonardo da Vinci - Artysta
+# Simplicity is the ultimate sophistication
+
+# Albert Einstein - Naukowiec
+# Make everything as simple as possible, but not simpler
+
+# Steve Jobs - Biznesmen i projektant
+# Simplify, simplify, simplify
+
+
+# Jak działa KISS?
+
+# Wymyślenie czegoś prostego jest trudne.
+
+# Prostota jest optymalna.
+
+# Program z czasem komplikuje się.
+
+# Każdy kod da się uprosić, chyba że jest prosty.
+
+
+
